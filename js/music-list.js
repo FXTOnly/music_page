3d3a45523249caf5b75539c9e73189f177e112fd
FXTER <130070328+FXT-SYSTEM@users.noreply.github.com>
2024-01-02 08:45:18 +0000

    U

--- js/music-list.js
+++ js/music-list.js
@@ -17,4 +17,22 @@ let allMusic = [
     img: "music-3",
     src: "music-3"
   },
+  {
+    name: "Ai Đâu Hay",
+    artist: "Sean Poet",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Rồi Chúng Ta Sẽ Hạnh Phúc",
+    artist: "Jack",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ngày Mai Người Ta Đi Lấy Chồng",
+    artist: "Thành Đạt",
+    img: "music-6",
+    src: "music-6"
+  },
 ];
