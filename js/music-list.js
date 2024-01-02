bc989e75a10bc56d06406f786f8aa1129ca17c8b
FXTER <130070328+FXT-SYSTEM@users.noreply.github.com>
2024-01-02 02:41:34 +0000

    Update

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "Chạnh Lòng Thương Cô 4 | Minion",
+    artist: "Huy Vạc",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Đồng Cỏ Lau OST",
+    artist: "Nhã Chi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Để Dành Khi Thức Giấc",
+    artist: "Sivan",
+    img: "music-3",
+    src: "music-3"
+  },
+];
